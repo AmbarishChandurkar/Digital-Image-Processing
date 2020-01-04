4cd6ff792dcf2fd45f95fdb99cabd58816a545e3
EngineerOfECE <ambarishchdk@gmail.com>
2020-01-04 21:32:06 +0530

    Modified Plots to show Comparison and also saved their copies

--- Study of Histogram Equalisation/Demo_Script.m
+++ Study of Histogram Equalisation/Demo_Script.m
@@ -5,19 +5,25 @@ clc;clearvars;close all;
 %% Part 1 : Demo on the 8 X 8 Matrix as on Wikipedia
 WikiImage = imread('WikiImage.png');
 figure(1);
+subplot(121);
 imshow(WikiImage); %Show the original Image
+title('Original Image');
 
+subplot(122);
 WikiImage_HistEq = MyHistEq('WikiImage.png');
-figure(2);
 imshow(WikiImage_HistEq); %Show the Histogram Equalised Image
+title('Histogram Equalised Image');
 
 %% Part 2 : Demo on the Leena Image
 
 TestImage = imread('Lenna_(test_image).png');
 TestImage = rgb2gray(TestImage);
-figure(3);
+figure(2);
+subplot(121);
 imshow(TestImage); %Show the original Image
+title('Original Image of Leena');
 
+subplot(122);
 TestImage_HistEq = MyHistEq('Lenna_(test_image).png');
-figure(4);
 imshow(TestImage_HistEq); %Show the Histogram Equalised Image
+title('Histogram Equalised Image of Leena');
