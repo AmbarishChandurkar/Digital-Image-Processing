90442dad3c0dd520acf1e1d42e6fc1cb3f5d2342
EngineerOfECE <ambarishchdk@gmail.com>
2020-01-10 13:23:22 +0530

    Created a skeleton script of the expected program

---
+++ Study Of Image Compression/MainScript.m
@@ -0,0 +1,19 @@
+%% Author : Ambarish Prashant Chandurkar (BT17ECE021)
+%% Date : 10 January 2019
+%In this is Script following operations are done on a sample Image
+% 1) Display original Image
+% 2) Treat each bit of the 8 bit grayscale image as separate image plane 
+%    and plot each bit plane as separate 8 images.
+% 3) Finally combine all the bit image planes EXCEPT the LSB Image plane,
+%    and display the resultant image.
+% 4) Observe the effect of compression i.e removal of the LSB plane on the
+%   memory size and the quality of image w.r.t original. 
+clc;clearvars;close all;
+
+%% Part 1 : Display original Image
+
+%% Part 2 : Plotting of each Bit plane
+
+%% Part 3 : Making and Display of Compressed Image
+
+%% Part 4 : Comparison of Original and Compressed Image
